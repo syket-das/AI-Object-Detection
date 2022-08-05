0bdc33dcd613942c7ae7648e5826e7fa6adf4cf5
syket-das <saiketsd23@gmail.com>
2022-08-05 23:05:04 +0530

    s

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import cv2
 
 
-def reScaleFrame(frame, scale=0.75):
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-    dimensions = (width, height)
-    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+def reScaleFrame(frame):
+  
+    return cv2.resize(frame, (800, 600), interpolation = cv2.INTER_LINEAR) 
 
 
 
@@ -23,9 +21,8 @@ human_tracker = cv2.CascadeClassifier("fullbody.xml")
 
 while True:
     read_successful, frame = video.read()
-
     if read_successful:
-        frame_resized = reScaleFrame(frame, scale=.2)
+        frame_resized = reScaleFrame(frame)
         grayscaled_frame = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
         
     else:
@@ -43,7 +40,7 @@ while True:
 
     for (x,y,w,h) in humans:
         cv2.rectangle(frame_resized , (x,y), (x+w , y+h), (0,255,255), 2)
-        cv2.putText(frame_resized, "padestrain", (x-5, y-10), 0, 0.4, (0,255,0), 1)
+        cv2.putText(frame_resized, "human", (x-5, y-10), 0, 0.4, (0,255,0), 1)
 
 
 
